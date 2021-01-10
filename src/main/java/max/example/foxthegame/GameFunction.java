64e1764572877f5ef01ec3eea6d5847c53f031c9
Maxim_Isaenkov <m.isaenkov@gmail.com>
2021-01-10 21:33:09 +0300

    Fix critical bug with scanner

--- src/main/java/max/example/foxthegame/GameFunction.java
+++ src/main/java/max/example/foxthegame/GameFunction.java
@@ -28,23 +28,31 @@ public class GameFunction {
 
             System.out.println("Выберите действие:");
 
-            int var = scanner.nextInt();
-            scanner.nextLine();
+            if (scanner.hasNextInt()) {
 
-            switch (var) {
-                case 1:
-                    menu.start();
-                    break;
-                case 2:
-                    menu.load();
-                    break;
-                case 3:
-                    menu.exit();
-                    flag = false;
-                    break;
-                default:
-                    System.out.println("Нет такого варианта");
-                    break;
+                int var = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (var) {
+                    case 1:
+                        menu.start();
+                        break;
+                    case 2:
+                        menu.load();
+                        break;
+                    case 3:
+                        menu.exit();
+                        flag = false;
+                        break;
+                    default:
+                        System.out.println("Нет такого варианта");
+                        break;
+                }
+
+            } else {
+                System.out.println("Вводите только цифры!");
+                scanner.nextLine();
+                flag = true;
             }
 
         }
@@ -70,7 +78,7 @@ public class GameFunction {
 
         GameNode adventureTime = new GameNode();
         adventureTime.setNodeText("Все лесные жители были заняты своими делами и не обращали внимания на Лисёнка и его проблему.\n" +
-                "Но вдруг кто нибудь видел Бельчонка... Лисёнок не знал, что ему делать. Помогите ему.");
+                "Но вдруг кто-нибудь видел Бельчонка... Лисёнок не знал, что ему делать. Помогите ему.");
         adventureTime.setVarOne("Попытаться разузнать о Бельчонке у лесных жителей");
         adventureTime.setVarTwo("Искать Бельчонка в одиночку");
 
@@ -93,15 +101,15 @@ public class GameFunction {
 
         GameNode askOwl = new GameNode();
         askOwl.setNodeText("Сова долго не хотела говорить, но в итоге сказала, что видела испуганного Бельчонка, \n" +
-                "бежавшего в глубь леса. Все лесные жители знают, что в глубине леса опасно, если Бельчонок там, \n" +
+                "бежавшего вглубь леса. Все лесные жители знают, что в глубине леса опасно! Если Бельчонок там, \n" +
                 "ему срочно нужна помощь.");
-        askOwl.setVarOne("Поверить Сове и отправиться в глубь леса");
-        askOwl.setVarTwo("Сове не стоит верить->Искать Бельчонка в одиночку");
+        askOwl.setVarOne("Поверить Сове и отправиться вглубь леса");
+        askOwl.setVarTwo("Сове не стоит верить -> Искать Бельчонка в одиночку");
 
         GameNode askWolf = new GameNode();
         askWolf.setNodeText("Волк оказался вполне дружелюбным, но помочь не смог. Лишь сказал, \n" +
                 "что маленькому лисенку не стоит бродить по лесу одному. И как теперь поступить?");
-        askWolf.setVarOne("Волк прав->Вернуться домой");
+        askWolf.setVarOne("Волк прав -> Вернуться домой");
         askWolf.setVarTwo("Искать Бельчонка в одиночку");
 
         tryAskAbout.setNodeOne(askOwl);
@@ -110,7 +118,7 @@ public class GameFunction {
         GameNode trustOwl = new GameNode();
         trustOwl.setNodeText("В глубине леса Лисёнок встретил Медвежонка. Ленивый Медвежонок был готов рассказать все, \n" +
                 "что знает, если Лисёнок принесёт ему мёда.");
-        trustOwl.setVarOne("Нет, потрачено слишком много времени, нужно идти дальше->Искать Бельчонка в одиночку");
+        trustOwl.setVarOne("Нет, потрачено слишком много времени, нужно идти дальше -> Искать Бельчонка в одиночку");
         trustOwl.setVarTwo("Нужно воспользоваться шансом и раздобыть мёд");
 
         askOwl.setNodeOne(trustOwl);
@@ -129,28 +137,28 @@ public class GameFunction {
         trustOwl.setNodeTwo(takeHoney);
 
         GameNode waitingBeesFlyAway = new GameNode();
-        waitingBeesFlyAway.setNodeText("Лисёнок подождал немного, и пчёлы разлетелись. Лисёнок без проблем набрал мёда. \n" +
+        waitingBeesFlyAway.setNodeText("Лисёнок подождал немного и пчёлы разлетелись. Лисёнок без проблем набрал мёда. \n" +
                 "Вдруг он понял, что очень голоден. Что же делать?");
         waitingBeesFlyAway.setVarOne("Поесть немного и передохнуть");
         waitingBeesFlyAway.setVarTwo("Скорее отнести мёд Медвежонку");
 
         GameNode robberHoneyImmediately = new GameNode();
         robberHoneyImmediately.setNodeText("Это была не лучшая идея. Пчёлы покусали Лисёнка, теперь ему самому нужна помощь. \n" +
-                "Игра закончилась неудачей");
+                "Игра закончилась неудачей.");
 
         takeHoney.setNodeOne(waitingBeesFlyAway);
         takeHoney.setNodeTwo(robberHoneyImmediately);
 
         GameNode EatAndRest = new GameNode();
         EatAndRest.setNodeText("Пока Лисёнок ел, злобные пчёлы вернулись и покусали его. Лисёнку нужна помощь, \n" +
-                "он не сможет продолжить поиски. Игра завершилась неудачей");
+                "он не сможет продолжить поиски. Игра завершилась неудачей.");
 
         GameNode bringHoneyFast = new GameNode();
         bringHoneyFast.setNodeText("Довольный Медвежонок рассказал Лисёнку, что очень хорошо знает семью Белок и уверен, \n" +
                 "что Бельчонок никогда не пошёл бы в глубь леса. Он заверял Лисёнка, что Белки не попадают в неприятности, \n" +
                 "и что Совам нельзя верить, он также уговаривал Лисёнка вернуться домой.");
-        bringHoneyFast.setVarOne("Медвежонок ничего не знает, нужно продолжить поиски->Искать Бельчонка в одиночку");
-        bringHoneyFast.setVarTwo("Может быть он прав и Лисёнок просто паникует->Вернуться домой");
+        bringHoneyFast.setVarOne("Медвежонок ничего не знает, нужно продолжить поиски -> Искать Бельчонка в одиночку");
+        bringHoneyFast.setVarTwo("Может быть он прав и Лисёнок просто паникует -> Вернуться домой");
 
         waitingBeesFlyAway.setNodeOne(EatAndRest);
         waitingBeesFlyAway.setNodeTwo(bringHoneyFast);
--- src/main/java/max/example/foxthegame/GameNode.java
+++ src/main/java/max/example/foxthegame/GameNode.java
@@ -80,59 +80,68 @@ public class GameNode implements Runnable, Serializable {
             menu = new Menu();
             menu.setExitGameCommand(exitGameCommand);
 
-            System.out.println(varOne);
-            System.out.println(varTwo);
-            System.out.println();
-            System.out.println(varThree);
-            System.out.println(varFour);
-
             Scanner scanner = new Scanner(System.in);
 
             boolean flag = true;
             while (flag) {
 
+                System.out.println(varOne);
+                System.out.println(varTwo);
+                System.out.println();
+                System.out.println(varThree);
+                System.out.println(varFour);
+
                 System.out.println("Выберите действие:");
-                int var = scanner.nextInt();
-                scanner.nextLine();
 
-                Thread newThread = null;
+                if (scanner.hasNextInt()) {
 
-                switch (var) {
-                    case 1:
-                        newThread = new Thread(nodeOne);
-                        newThread.start();
-                        try {
-                            newThread.join();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        flag = false;
-                        break;
-                    case 2:
-                        newThread = new Thread(nodeTwo);
-                        newThread.start();
-                        try {
-                            newThread.join();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        flag = false;
-                        break;
-                    case 3:
-                        GameFunction.saveGame(nodeText, varOne, varTwo, nodeOne, nodeTwo);
-                        break;
-                    case 4:
-                        menu.exit();
-                        flag = false;
-                        break;
-                    default:
-                        System.out.println("Нет такого варианта");
+                    int var = scanner.nextInt();
+                    scanner.nextLine();
+
+                    Thread newThread = null;
+
+                    switch (var) {
+                        case 1:
+                            newThread = new Thread(nodeOne);
+                            newThread.start();
+                            try {
+                                newThread.join();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            flag = false;
+                            break;
+                        case 2:
+                            newThread = new Thread(nodeTwo);
+                            newThread.start();
+                            try {
+                                newThread.join();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            flag = false;
+                            break;
+                        case 3:
+                            GameFunction.saveGame(nodeText, varOne, varTwo, nodeOne, nodeTwo);
+                            break;
+                        case 4:
+                            menu.exit();
+                            flag = false;
+                            break;
+                        default:
+                            System.out.println("Нет такого варианта");
+                    }
+
+                } else {
+                    System.out.println("Вводите только цифры!");
+                    scanner.nextLine();
+                    System.out.println(nodeText);
+                    flag = true;
                 }
 
             }
 
         }
-
 
     }
 
